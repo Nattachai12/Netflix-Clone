ba9087577f76a1ecb28de86dabeca4239513b748
Nattachai12 <nattachainudol@gmail.com>
2022-02-27 11:29:37 -0800

    add banner

---
+++ src/Banner.jsx
@@ -0,0 +1,54 @@
+import React, {useState, useEffect} from 'react';
+import axios from './axios';
+import requests from './requests';
+import './banner.css';
+function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      const random = Math.floor(Math.random() * (request.data.results.length));
+      setMovie(
+        request.data.results[random]
+      )
+    }
+    fetchData();
+  }, [])
+
+  function truncate(str, num) {
+    return str?.length > num ? str.substr(0, num - 1) + '...' : str;
+  }
+
+  return (
+    //header is for Background Img
+    <header className='banner'
+      style={{
+        backgroundSize: 'cover',
+        backgroundImage: `url(
+          "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
+        )`,
+        backgroundPosition: 'center center'
+      }}
+    >
+      <div className='banner__contents'>
+        <h1 className="banner__title">
+          {movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        {/* div > two buttons */}
+        {/* description */}
+        <div>
+          <h1 className="banner__description">
+            {truncate(movie?.overview, 350)}
+          </h1>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Banner;
--- src/Row.jsx
+++ src/Row.jsx
@@ -1,27 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
+import './Row.css';
 
-function Row({ title, fetchURL }) {
+
+const base_url = 'https://image.tmdb.org/t/p/original/';
+
+function Row({ title, fetchURL, isLargeRow }) {
   const [movies, setMovies] = useState([]);
   // If second argument is an empty array, meaning the useEffet func willl run run when Row is created.
   //If there is an element inside an empty array, useEffect
   // will run when the state of the element change.
   useEffect(() => {
     async function fetchData() {
+      //we get the Url from get request
       const request = await axios.get(fetchURL);
-      // console.log('Movies: ', request.data.results);
       setMovies(request.data.results);
       return request;
     }
     fetchData();
-  }, []);
-
+  }, [fetchURL]);
 
   return (
-    <div>
+    <div className='row'>
       {/*  title */}
       <h2>{title}</h2>
-      {/* container -> posters */}
+      <div className='row__posters'>
+        {/* row_poster*/}
+        {movies.map(movie => (
+          <img
+            key={movie.id}
+              className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
+              src={`${base_url}${isLargeRow? movie.poster_path : movie.backdrop_path}`}
+              alt={movie.name}
+          />
+        ))}
+      </div>
     </div>
   )
 }
